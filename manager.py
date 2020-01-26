9c5c88c805e2d6c2732c915a7252c97e668acec3
redrik234 <illarionow.artem@yandex.ru>
2020-01-26 23:51:33 +0300

    * Дописан менеджер

--- manager.py
+++ manager.py
@@ -1,37 +1,152 @@
+import random
+
+
 class Manager:
     # === Данные сессии ===
-    _user_id = 2242
-    _case_id = 2  # кейсы лабиринта
-    _tournament_id = 0  # id турнира
-    _hash_id = 0  # id контрольной игры
+    __user_id = None  # id пользователя
+    __case_id = None  # кейсы лабиринта
+    __tournament_id = 0  # id турнира
+    __hash_id = 0  # id контрольной игры
 
     # === Данные по картам ===
-    _map_nums = []  # номера карт
-    _attempts_count = 5  # количество попыток на одн карту
-    _total_game_count = 10  # общее количество игр
+    __map_nums = []  # номера карт
+    __attempts_count = 5  # количество попыток на одн карту
+    __total_games_count = 10  # общее количество игр
 
     # === Параметры обучения агента===
-    _alpha = 0  # фактор обучения
-    _gamma = 0  # фактор дисконтирования
-    _delta = 0  # коэффициент уменьшения alpha
-    _batch_size = 10  # размер пакета обучения
-    _loss = 0  # ошибка выбора действия агента
+    __alpha = 0  # фактор обучения
+    __gamma = 0  # фактор дисконтирования
+    __delta = 0  # коэффициент уменьшения alpha
+    __batch_size = 10  # размер пакета обучения
+    __loss = 0  # ошибка выбора действия агента
+
+    # === Параметры для подсчета результатов игр ===
+    __games_count = 0
+    __wins_count = 0
+    __score_count = 0
+    __lost_map_list = []
 
     # === Конструктор менеджера игр ===
     # session_data = [user_id, case_id, tournament_id, hash_id]
     # map_data = [map_nums, attempts_count, total_game_count]
     # nnet_data = [alpha, gamma. delta, batch_size]
     def __init__(self, session_data, map_data, nnet_data):
-        self._user_id = session_data[0]
-        self._case_id = session_data[1]
-        self._tournament_id = session_data[2]
-        self._hash_id = session_data[3]
+        self.__user_id = session_data[0]
+        self.__case_id = session_data[1]
+        self.__tournament_id = session_data[2]
+        self.__hash_id = session_data[3]
 
-        self._map_nums = map_data[0]
-        self._attempts_count = map_data[1]
-        self._total_game_count = map_data[2]
+        self.__map_nums = map_data[0]
+        self.__attempts_count = map_data[1]
+        self.__total_games_count = map_data[2]
 
-        self._alpha = nnet_data[0]
-        self._gamma = nnet_data[1]
-        self._delta = nnet_data[2]
-        self._batch_size = nnet_data[3]
+        self.__alpha = nnet_data[0]
+        self.__gamma = nnet_data[1]
+        self.__delta = nnet_data[2]
+        self.__batch_size = nnet_data[3]
+
+    # === Метод для обучения интеллектуального агента ===
+    def train(self, agent, iter_count=1):
+        self.__print_beginning_of_games(iter_count)
+
+        self.__lost_map_list = []
+
+        for it in range(1, iter_count + 1):
+            self.__games_count = 0
+            self.__wins_count = 0
+            self.__score_count = 0
+
+            agent = self.__play(agent, it)
+
+            self.__total_games_count += self.__games_count
+            win_rate = (self.__wins_count * 100) / self.__games_count
+            score_rate = self.__score_count / self.__games_count
+            self.__print_overall_result(win_rate, score_rate)
+            agent.save_nnet()
+
+    def __play(self, agent, iteration):
+        random.shuffle(self.__map_nums)
+        for map_num in range(len(self.__map_nums)):
+            for attempt_num in range(1, self.__attempts_count + 1):
+                hash_id, map_id = self.__get_hash_and_map(map_num)
+                # TODO дописать параметры метода вызова агента
+                code, score = agent.playGame()
+                if code is None:
+                    print("Connection failed for: map = {0}, attempt = {1}".format(map_id, attempt_num))
+                else:
+                    self.__update_games_result(code, score, map_id)
+
+                self.__print_game_result(map_num, attempt_num, iteration)
+
+                self.__alpha -= self.__delta
+                if self.__alpha < 0.01:
+                    self.__alpha = 0.01
+
+        return agent
+
+    def __print_overall_result(self, win_rate, score_rate):
+        games_info = "*Games: ", self.__games_count
+        win_info = "*Win rate: {:6.2f}, Score rate: {:6.2f}" . format(win_rate, score_rate)
+        lost_info = "*Lost Game Maps: ".join(self.__lost_map_list)
+        str_length = 0
+        if len(win_info) > len(lost_info):
+            str_length = len(win_info) + 1
+        else:
+            str_length = len(lost_info) + 1
+
+        print('*' * (str_length + 1))
+        s = ' ' * (str_length - len(games_info)) + '*'
+        print(games_info + s)
+        s = ' ' * (str_length - len(win_info)) + '*'
+        print(win_info + s)
+        s = ' ' * (str_length - len(lost_info)) + '*'
+        print(lost_info + s)
+        print('*' * (str_length + 1))
+
+    def __print_game_result(self, map_num, attempt_num, iteration):
+        print('*' * 100)
+        if self.__tournament_id == 0:
+            print(
+                "map_num: ", map_num + 1,
+                ", attempt: ", attempt_num,
+                ", wins=", self.__wins_count,
+                ", alpha = {:8.6f}".format(self.__alpha)
+            )
+        else:
+            print(
+                "Iteration: ", iteration,
+                ", tid: ", self.__tournament_id,
+                ", wins=", self.__wins_count,
+                ", alpha = {:8.6f}".format(self.__alpha)
+            )
+
+        print('*' * 100)
+
+    def __print_beginning_of_games(self, iter_count):
+        print('=' * 80)
+        print(' ' * 15 + "!!!Start of torture for the agent!!!")
+        print(
+            "Iteration count: ", iter_count,
+            ", attempt count: ", self.__attempts_count,
+            ", total game count: ", self.__total_games_count
+        )
+        print('=' * 80)
+
+    def __get_hash_and_map(self, map_num):
+        hash_id, map_id = None, None
+        if self.__hash_id == 0:
+            hash_id = 0
+            map_id = self.__map_nums[map_num]
+        else:
+            hash_id = self.__hash_id[map_num]
+            map_id = 0
+        return hash_id, map_id
+
+    def __update_games_result(self, code, score, map_num):
+        if code == 2:
+            self.__wins_count += 1
+            self.__score_count += score
+        else:
+            self.__lost_map_list.append(map_num)
+
+        self.__games_count += 1
